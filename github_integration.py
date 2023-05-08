52ecde3a7543ba630fbb5b1b74a40c6d0f50128c
Konrad Miotlinski <konrad.miotlinski@gmail.com>
2023-05-08 21:12:01 +0800

    Added Pedro and Thomas

--- github_integration.py
+++ github_integration.py
@@ -6,6 +6,6 @@ if today.month == 3 and today.day == 9:
     print('It is my birthday!')
 
 elif today.month == 8 and today.day == 31:
-    print("It is Thais' birthday!")    
+    print("It is Thais' birthday!")
 else:
     print("It is neither mine or Thais' birthday")
