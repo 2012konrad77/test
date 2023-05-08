1a710358f59b660caf744497e3194f3d6232d5a2
Konrad Miotlinski <konrad.miotlinski@gmail.com>
2023-05-08 21:44:14 +0800

    Minor bug fixes

--- github_integration.py
+++ github_integration.py
@@ -4,8 +4,11 @@ today = dt.date.today()
 
 if today.month == 3 and today.day == 9:
     print('It is my birthday!')
-
 elif today.month == 8 and today.day == 31:
     print("It is Thais' birthday!")
+elif today.month == 6 and today.day == 18:
+    print("It is Pedro's birthday!")
+elif today.month == 4 and today.day == 26:
+    print("It is Thomas' birthday!")
 else:
-    print("It is neither mine or Thais' birthday")
+    print("Today is neither mine, Thais', Pedro's or Thomas' birthday")
