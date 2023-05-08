5f6580aff26571cfb90656d31d29685832a9e8bd
Konrad Miotlinski <konrad.miotlinski@gmail.com>
2023-05-08 20:35:46 +0800

    Thais description added

--- github_integration.py
+++ github_integration.py
@@ -4,6 +4,9 @@ today = dt.date.today()
 
 if today.month == 3 and today.day == 9:
     print('It is my birthday!')
+
+elif today.month == 8 and today.day == 31:
+    print('It is Thais birthday!')    
 else:
-    print('It is not my birthday!')
+    print('It is neither my or Thais birthday')
 
